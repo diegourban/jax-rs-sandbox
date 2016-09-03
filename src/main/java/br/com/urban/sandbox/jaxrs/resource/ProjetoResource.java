24c27e52cb63cda848f3a5f2de76f9416c2f9fc4
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-03 14:07:31 -0300

    REST WS and Addressability

---
+++ src/main/java/br/com/urban/sandbox/jaxrs/resource/ProjetoResource.java
@@ -0,0 +1,20 @@
+package br.com.urban.sandbox.jaxrs.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.urban.sandbox.jaxrs.dao.ProjetoDAO;
+import br.com.urban.sandbox.jaxrs.modelo.Projeto;
+
+@Path("projetos")
+public class ProjetoResource {
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public String busca() {
+		Projeto projeto = new ProjetoDAO().busca(1l);
+		return projeto.toXML();
+	}
+}
---
+++ src/test/java/br/com/urban/sandbox/jaxrs/ClienteTest.java
@@ -0,0 +1,29 @@
+package br.com.urban.sandbox.jaxrs;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Test;
+
+public class ClienteTest {
+	
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.mocky.io");
+		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+		assertTrue(conteudo.contains("Rua Vergueiro 3185"));
+	}
+	
+	@Test
+    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/projetos").request().get(String.class);
+        assertTrue(conteudo.contains("<nome>Minha loja"));
+    }
+
+}
