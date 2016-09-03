a504949c4ca207568fe7eab39a8ae3701fbd01db
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-03 16:02:58 -0300

    end-to-end tests with JAX-RS and Grizzly

--- src/main/java/br/com/urban/sandbox/jaxrs/Servidor.java
+++ src/main/java/br/com/urban/sandbox/jaxrs/Servidor.java
@@ -10,12 +10,17 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class Servidor {
 
 	public static void main(String[] args) throws IOException {
-		URI uri = URI.create("http://localhost:8080/");
-		ResourceConfig config = new ResourceConfig().packages("br.com.urban.sandbox.jaxrs");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		HttpServer server = inicializarServidor();
 		System.out.println("Servidor rodando");
 		System.in.read();
 		server.shutdownNow();
+	}
+
+	static HttpServer inicializarServidor() {
+		URI uri = URI.create("http://localhost:8080/");
+		ResourceConfig config = new ResourceConfig().packages("br.com.urban.sandbox.jaxrs");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
 	}
 
 }
--- src/test/java/br/com/urban/sandbox/jaxrs/ClienteTest.java
+++ src/test/java/br/com/urban/sandbox/jaxrs/ClienteTest.java
@@ -1,29 +1,51 @@
 package br.com.urban.sandbox.jaxrs;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.urban.sandbox.jaxrs.modelo.Carrinho;
+import br.com.urban.sandbox.jaxrs.modelo.Projeto;
 
 public class ClienteTest {
 	
-	@Test
-	public void testaQueAConexaoComOServidorFunciona() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		assertTrue(conteudo.contains("Rua Vergueiro 3185"));
+	private HttpServer server;
+
+	@Before
+	public void before() {
+		server = Servidor.inicializarServidor();
+	}
+	
+	@After
+	public void after() {
+		server.shutdownNow();
 	}
 	
 	@Test
-    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+	public void deveBuscarOCarrinhoEsperado() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+    public void deveBuscarOProjetoEsperado() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
         String conteudo = target.path("/projetos").request().get(String.class);
-        assertTrue(conteudo.contains("<nome>Minha loja"));
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        assertEquals("Minha loja", projeto.getNome());
     }
 
 }
